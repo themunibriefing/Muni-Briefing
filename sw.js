e7f86baac121748f6cc9dd2f5b995a0f25e54cda
CampusNews <muhimatim51@gmail.com>
2026-09-01 15:19:33 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'muni-briefing-shell-v1';
+const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_FILES)).catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network first, falling back to cache (so stories stay fresh, shell still loads offline)
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
